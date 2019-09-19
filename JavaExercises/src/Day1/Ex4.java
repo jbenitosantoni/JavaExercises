fc03268a01d5ad806707d4ef76ffbf8f29e3e353
Javier <javierbenitosantoni@icloud.com>
2019-09-19 16:36:27 +0200

    Ejercicios hechos

--- JavaExercises/src/Day1/Ex4.java
+++ JavaExercises/src/Day1/Ex4.java
@@ -1,10 +1,15 @@
 package Day1;
 
+import java.util.Scanner;
+
 public class Ex4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Radio de circulo");
+		double radio = sc.nextDouble();
+		double area = Math.pow (radio, 2) * Math.PI;
+		System.out.println("El area del circulo es: " + area);
 	}
 
 }

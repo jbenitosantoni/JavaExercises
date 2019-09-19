8a646ccd480c2779865be1921132db85a6194aef
Javier <javierbenitosantoni@icloud.com>
2019-09-19 16:08:47 +0200

    a

---
+++ JavaExercises/src/Day1/Ex2.java
@@ -0,0 +1,19 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un numero");
+		double num1 = sc.nextDouble();
+		System.out.println("Introduce otro numero");
+		double num2 = sc.nextDouble();
+		System.out.println("Introduce otro numero");
+		double num3 = sc.nextDouble();
+		double operacion = Math.pow(Math.pow(num1, num2), num3);
+		System.out.print("El resultado es: " + operacion);
+	}
+
+}

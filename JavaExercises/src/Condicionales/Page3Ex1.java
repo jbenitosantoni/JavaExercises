d66057bc6d5db4a6e64958fca4a07cb02e9d29d0
javierbenitosantoni <javierbenitosantoni@192.168.0.17>
2019-09-23 19:36:59 +0200

    Ejercicios Condicionales Terminados

---
+++ JavaExercises/src/Condicionales/Page3Ex1.java
@@ -0,0 +1,24 @@
+package Condicionales;
+
+import java.util.Scanner;
+
+public class Page3Ex1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Numero de jugadores que necesitas");
+		int jugadores = sc.nextInt();
+		double presupuesto = jugadores * 2000000;
+		System.out.println("Numero de jugadores comunitarios");
+		int jugadorescomunitarios = sc.nextInt();
+		System.out.println("Numero de jugadores extranjeros");
+		int jugadoresextranjeros	 = sc.nextInt();
+		if (jugadorescomunitarios > 5) {
+			presupuesto = presupuesto - 5000000;
+		} if (jugadoresextranjeros >= 2) {
+			presupuesto = presupuesto - 5000000;
+		}
+		System.out.print("Dispones de " + presupuesto + " euros de presupuesto.");
+	}
+
+}
---
+++ JavaExercises/src/Condicionales/Page3Ex4.java
@@ -0,0 +1,18 @@
+package Condicionales;
+
+import java.util.Scanner;
+
+public class Page3Ex4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nombre");
+		String nombre = sc.nextLine();
+		if (nombre.contentEquals("Antonio") || nombre.contentEquals("Pepe") || nombre.contentEquals("Juan") || nombre.contentEquals("Carlos")) {
+			System.out.println("Serás expulsado del centro");
+		} else {
+			System.out.print("Bienvenido");
+		}
+	}
+
+}

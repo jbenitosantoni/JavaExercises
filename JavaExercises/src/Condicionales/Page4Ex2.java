136fd4b104f3a76d883c0d1959adc67e70b6785e
Javier Benito Santoni <javierbenitosantoni@icloud.com>
2019-09-24 13:54:26 +0200

    Ejercicios Hechos en Clase

---
+++ JavaExercises/src/Condicionales/Page4Ex2.java
@@ -0,0 +1,21 @@
+package Condicionales;
+
+import java.util.Scanner;
+
+public class Page4Ex2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número");
+		double num1 = sc.nextDouble();
+		double num2 = sc.nextDouble();
+		double num3 = sc.nextDouble();
+		double num4 = sc.nextDouble();
+		if (num1 % 2 == 0 && num2 % 2 == 0 && num3 % 2 == 0 && num4 == 0) {
+			System.out.print("Todos son pares");
+		}else if(num1 % 2 != 0 && num2 % 2 != 0 && num3 % 2 != 0 && num4 != 0){
+			System.out.print("Todos son impares");
+		}
+		sc.close();
+	}
+}

0294af65c120a0049c2a1690d6e1d1b2271d9d5e
Javier Benito Santoni <javierbenitosantoni@icloud.com>
2019-09-25 12:54:18 +0200

    Fallo

--- JavaExercises/src/Condicionales/Page5Ex4.java
+++ JavaExercises/src/Condicionales/Page5Ex4.java
@@ -8,8 +8,12 @@ public class Page5Ex4 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce un número entero");
 		int num1 = sc.nextInt();
+		if (num1 <= 100) {
 		double cien = 100 - num1;
 		System.out.print("A el número introducido le quedan " + cien + " unidades para llegar a cien.");
+		} else {
+			System.out.print("El número es mayor de 100");
+		}
 		sc.close();
 	}
 

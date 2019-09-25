21c3f8299d18a2c5ccf219f70a64d862c9694598
javierbenitosantoni <javierbenitosantoni@192.168.0.17>
2019-09-25 16:56:40 +0200

    El dos estaba mal

--- JavaExercises/src/Condicionales/Page6Ex2.java
+++ JavaExercises/src/Condicionales/Page6Ex2.java
@@ -10,42 +10,26 @@ public class Page6Ex2 {
 		double num1 = sc.nextDouble();
 		double num2 = sc.nextDouble();
 		double num3 = sc.nextDouble();
-		double grande = 0;
-		double mediano = 0;
-		double pequeño = 0;
-		if (num1 > num2 && num3 < num1) {
-			grande = num1;
-			if (num2 > num3) {
-				mediano = num2;
-				pequeño = num3;
-				System.out.print(grande + ", " + mediano + ", " + pequeño);
-			} else {
-				mediano = num3;
-				pequeño = num2;
-				System.out.print(grande + ", " + mediano + ", " + pequeño);
-			}
-		} else if (num2 > num1 && num3 < num2) {
-			grande = num2;
-			if (num3 > num1) {
-				mediano = num3;
-				pequeño = num1;
-				System.out.print(grande + ", " + mediano + ", " + pequeño);
-			} else {
-				mediano = num1;
-				pequeño = num3;
-				System.out.print(grande + ", " + mediano + ", " + pequeño);
-			}
-		} else if (num3 > num1 && num1 < num3) {
-			grande = num3;
-			if (num2 > num1) {
-				mediano = num2;
-				pequeño = num1;
-				System.out.print(grande + ", " + mediano + ", " + pequeño);
-			} else {
-				mediano = num1;
-				pequeño = num2;
-				System.out.print(grande + ", " + mediano + ", " + pequeño);
-			}
+		if (num1 > num2 && num2 > num3) {
+			System.out.print(num1 + ", " + num2 + ", " + num3);
+		} else if (num2 > num3 && num3 > num1) {
+			System.out.print(num2 + ", " + num3 + ", " + num1);
+		} else if (num3 > num2 && num2 > num1) {
+			System.out.print(num3 + ", " + num2 + ", " + num1);
+		} else if (num3 > num1 && num1 > num2) {
+			System.out.print(num3 + ", " + num1 + ", " + num2);
+		} else if (num2 > num1 && num1 > num3) {
+			System.out.print(num2 + ", " + num1 + ", " + num3);
+		} else if (num1 > num3 && num3 > num2) {
+			System.out.print(num1 + ", " + num3 + ", " + num2);
+		} else if (num1 == num2 && num2 > num3) {
+			System.out.print(num1 + ", " + num2 + ", " + num3);
+		} else if (num3 == num2 && num3 > num3) {
+			System.out.print(num3 + ", " + num2 + ", " + num1);
+		}else if (num1 == num3 && num3 > num2) {
+			System.out.print(num1 + ", " + num3 + ", " + num2);
+		} else {
+			System.out.print("Todos los números son iguales");
 		}
 		sc.close();
 	}

f47f981af5c20db20358f5361440e5ec8a60f415
Javier Benito Santoni <javierbenitosantoni@icloud.com>
2019-10-17 13:13:08 +0200

    Juego tablero terminado

--- JavaExercises/src/ejercicio_code_week/Tablero.java
+++ JavaExercises/src/ejercicio_code_week/Tablero.java
@@ -28,13 +28,15 @@ public class Tablero {
 		String j2[] = new String[10];
 		int coordenada1 = 0;
 		int coordenada2 = 0;
+		int contadoralianza = 0;
+		int contadorimperio = 0;
 		int intro = 8;
 		do {
-			System.out.println("1. Iniciar o Resetear");
+			System.out.println("\n1. Iniciar o Resetear");
 			System.out.println("2. Colocar naves Alianza");
 			System.out.println("3. Colocar naves Imperio");
 			System.out.println("4. Atacar nave Alianza");
-			System.out.println("5. Atacar nave Alianza");
+			System.out.println("5. Atacar nave Imperio");
 			System.out.println("6. Dibujar tablas");
 			System.out.println("7. Salir");
 			intro = sc.nextInt();
@@ -61,6 +63,215 @@ public class Tablero {
 					h2[i] = "0";
 					i2[i] = "0";
 					j2[i] = "0";
+				}
+				break;
+			case 2:
+				for (int i = 0; i < 4; i++) {
+					System.out.println("Dame 2 coordenadas de nave de alianza");
+					coordenada1 = sc.nextInt();
+					coordenada2 = sc.nextInt();
+					if (coordenada2 == 0) {
+						a1[coordenada1] = "H";
+					} else if (coordenada2 == 1) {
+						b1[coordenada1] = "H";
+					} else if (coordenada2 == 2) {
+						c1[coordenada1] = "H";
+					} else if (coordenada2 == 3) {
+						d1[coordenada1] = "H";
+					} else if (coordenada2 == 4) {
+						e1[coordenada1] = "H";
+					} else if (coordenada2 == 5) {
+						f1[coordenada1] = "H";
+					} else if (coordenada2 == 6) {
+						g1[coordenada1] = "H";
+					} else if (coordenada2 == 7) {
+						h1[coordenada1] = "H";
+					} else if (coordenada2 == 8) {
+						i1[coordenada1] = "H";
+					} else if (coordenada2 == 9) {
+						j1[coordenada1] = "H";
+					}
+				}
+				break;
+			case 3:
+				for (int i = 0; i < 4; i++) {
+					System.out.println("Dame 2 coordenadas de nave de imperio");
+					coordenada1 = sc.nextInt();
+					coordenada2 = sc.nextInt();
+					if (coordenada2 == 0) {
+						a2[coordenada1] = "H";
+					} else if (coordenada2 == 1) {
+						b2[coordenada1] = "H";
+					} else if (coordenada2 == 2) {
+						c2[coordenada1] = "H";
+					} else if (coordenada2 == 3) {
+						d2[coordenada1] = "H";
+					} else if (coordenada2 == 4) {
+						e2[coordenada1] = "H";
+					} else if (coordenada2 == 5) {
+						f2[coordenada1] = "H";
+					} else if (coordenada2 == 6) {
+						g2[coordenada1] = "H";
+					} else if (coordenada2 == 7) {
+						h2[coordenada1] = "H";
+					} else if (coordenada2 == 8) {
+						i2[coordenada1] = "H";
+					} else if (coordenada2 == 9) {
+						j2[coordenada1] = "H";
+					}
+				}
+				break;
+			case 4:
+				System.out.println("Dame 2 coordenadas para atacar a alianza");
+				coordenada1 = sc.nextInt();
+				coordenada2 = sc.nextInt();
+				if (coordenada2 == 0) {
+					if (a1[coordenada1].equalsIgnoreCase("H")) {
+						a1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						a1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 1) {
+					if (b1[coordenada1].equalsIgnoreCase("H")) {
+						b1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						b1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 2) {
+					if (c1[coordenada1].equalsIgnoreCase("H")) {
+						c1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						c1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 3) {
+					if (d1[coordenada1].equalsIgnoreCase("H")) {
+						d1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						d1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 4) {
+					if (e1[coordenada1].equalsIgnoreCase("H")) {
+						e1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						e1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 5) {
+					if (f1[coordenada1].equalsIgnoreCase("H")) {
+						f1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						f1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 6) {
+					if (g1[coordenada1].equalsIgnoreCase("H")) {
+						g1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						g1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 7) {
+					if (h1[coordenada1].equalsIgnoreCase("H")) {
+						h1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						h1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 8) {
+					if (i1[coordenada1].equalsIgnoreCase("H")) {
+						i1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						i1[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 9) {
+					if (j1[coordenada1].equalsIgnoreCase("H")) {
+						j1[coordenada1] = "X";
+						contadorimperio++;
+					} else {
+						j1[coordenada1] = "A";
+					}
+				}
+				break;
+			case 5:
+				System.out.println("Dame 2 coordenadas para atacar a imperio");
+				coordenada1 = sc.nextInt();
+				coordenada2 = sc.nextInt();
+				if (coordenada2 == 0) {
+					if (a2[coordenada1].equalsIgnoreCase("H")) {
+						a2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						a2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 1) {
+					if (b2[coordenada1].equalsIgnoreCase("H")) {
+						b2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						b2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 2) {
+					if (c2[coordenada1].equalsIgnoreCase("H")) {
+						c2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						c2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 3) {
+					if (d2[coordenada1].equalsIgnoreCase("H")) {
+						d2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						d2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 4) {
+					if (e2[coordenada1].equalsIgnoreCase("H")) {
+						e2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						e2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 5) {
+					if (f2[coordenada1].equalsIgnoreCase("H")) {
+						f2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						f2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 6) {
+					if (g2[coordenada1].equalsIgnoreCase("H")) {
+						g2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						g2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 7) {
+					if (h2[coordenada1].equalsIgnoreCase("H")) {
+						h2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						h2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 8) {
+					if (i2[coordenada1].equalsIgnoreCase("H")) {
+						i2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						i2[coordenada1] = "A";
+					}
+				} else if (coordenada2 == 9) {
+					if (j2[coordenada1].equalsIgnoreCase("H")) {
+						j2[coordenada1] = "X";
+						contadoralianza++;
+					} else {
+						j2[coordenada1] = "A";
+					}
+					
 				}
 				break;
 			case 6:
@@ -144,67 +355,17 @@ public class Tablero {
 				for (int i = 0; i < a1.length; i++) {
 					System.out.print("_" + j2[i] + "_" + "|");
 				}
-				System.out.println("\n");
 				break;
-			case 2:
-				for (int i = 0; i < 4; i++) {
-					System.out.println("Dame 2 coordenadas de nave de alianza");
-					coordenada1 = sc.nextInt();
-					coordenada2 = sc.nextInt();
-					if (coordenada2 == 0) {
-						a1[coordenada1] = "H";
-					} else if (coordenada2 == 1) {
-						b1[coordenada1] = "H";
-					} else if (coordenada2 == 2) {
-						c1[coordenada1] = "H";
-					} else if (coordenada2 == 3) {
-						d1[coordenada1] = "H";
-					} else if (coordenada2 == 4) {
-						e1[coordenada1] = "H";
-					} else if (coordenada2 == 5) {
-						f1[coordenada1] = "H";
-					} else if (coordenada2 == 6) {
-						g1[coordenada1] = "H";
-					} else if (coordenada2 == 7) {
-						h1[coordenada1] = "H";
-					} else if (coordenada2 == 8) {
-						i1[coordenada1] = "H";
-					} else if (coordenada2 == 9) {
-						j1[coordenada1] = "H";
-					}
-				}
-				break;
-			case 3:
-				for (int i = 0; i < 4; i++) {
-					System.out.println("Dame 2 coordenadas de nave de alianza");
-					coordenada1 = sc.nextInt();
-					coordenada2 = sc.nextInt();
-					if (coordenada2 == 0) {
-						a2[coordenada1] = "H";
-					} else if (coordenada2 == 1) {
-						b2[coordenada1] = "H";
-					} else if (coordenada2 == 2) {
-						c2[coordenada1] = "H";
-					} else if (coordenada2 == 3) {
-						d2[coordenada1] = "H";
-					} else if (coordenada2 == 4) {
-						e2[coordenada1] = "H";
-					} else if (coordenada2 == 5) {
-						f2[coordenada1] = "H";
-					} else if (coordenada2 == 6) {
-						g2[coordenada1] = "H";
-					} else if (coordenada2 == 7) {
-						h2[coordenada1] = "H";
-					} else if (coordenada2 == 8) {
-						i2[coordenada1] = "H";
-					} else if (coordenada2 == 9) {
-						j2[coordenada1] = "H";
-					}
-				}
-				break;
+				
 			}
-		} while (intro != 7);
+		} while (intro != 7 && contadoralianza != 4 && contadorimperio != 4);
 		{
 		}
+		if (contadoralianza == 4) {
+			System.out.println("Ha ganado la alianza");
+		} if (contadorimperio == 4) {
+			System.out.println("Ha ganado el imperio");
+		}
+		sc.close();
 	}
 }

24f3d42ef4d6efa9a3898d26bde7386603c870d3
Javier Benito Santoni <javierbenitosantoni@icloud.com>
2019-11-05 14:08:40 +0100

    Put

--- JavaExercises/src/clases/ejercicio_clase_1/Alumno.java
+++ JavaExercises/src/clases/ejercicio_clase_1/Alumno.java
@@ -7,9 +7,11 @@ public class Alumno {
 	private String dni;
 	private int edad;
 	private double nota;
+
 	Alumno() {
-		
+
 	}
+
 	Alumno(String nombre, String apellidos, String n_expediente, String dni, int edad, double nota) {
 		super();
 		this.nombre = nombre;
@@ -19,46 +21,70 @@ public class Alumno {
 		this.edad = edad;
 		this.nota = nota;
 	}
+
 	Alumno(String nombre) {
 		super();
 		this.nombre = nombre;
 	}
-	
+
 	public String getApellidos() {
 		return apellidos;
 	}
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+
 	public String getN_expediente() {
 		return n_expediente;
 	}
+
 	public void setN_expediente(String n_expediente) {
 		this.n_expediente = n_expediente;
 	}
+
 	public String getDni() {
 		return dni;
 	}
+
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
 	public double getNota() {
 		return nota;
 	}
+
 	public void setNota(double nota) {
 		this.nota = nota;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getNombre() {
 		return this.nombre;
 	}
-	
+
+	void mostrarDatos() {
+		System.out.println("Nombre: " + this.nombre + " - Apellidos: " + this.apellidos + " - Expediente: "
+				+ this.n_expediente + " - DNI: " + this.dni + " - Edad: " + this.edad + " - Nota: " + this.nota);
+	}
+
+	public boolean aprobado() {
+		if (this.nota >= 7) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- JavaExercises/src/clases/ejercicio_clase_1/Main.java
+++ JavaExercises/src/clases/ejercicio_clase_1/Main.java
@@ -1,11 +1,35 @@
 package clases.ejercicio_clase_1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Alumno alumno1 = new Alumno("Javier", "Benito", "12", "02222222y", 20, 9.99);
-		Alumno alumno2 = new Alumno("Juan");
-		System.out.println(alumno1.getNombre() + " " + alumno1.getApellidos());
+		Scanner sc = new Scanner(System.in);
+		Scanner sn = new Scanner(System.in);
+		System.out.println("Introduce nombre alumno: ");
+		String nombre = sc.nextLine();
+		System.out.println("Introduce apellidos alumno: ");
+		String apellidos = sc.nextLine();
+		System.out.println("Introduce n_expediente alumno: ");
+		String n_expediente = sc.nextLine();
+		System.out.println("Introduce dni alumno: ");
+		String dni = sc.nextLine();
+		System.out.println("Introduce edad alumno: ");
+		int edad = sn.nextInt();
+		System.out.println("Introduce nota alumno: ");
+		Double nota = sn.nextDouble();
+
+		Alumno alumno3 = new Alumno(nombre, apellidos, n_expediente, dni, edad, nota);
+		alumno3.mostrarDatos();
+		if (alumno3.aprobado() == true) {
+			System.out.println("Aprobado");
+		} else {
+			System.out.println("Suspenso");
+		}
+		sc.close();
+		sn.close();
+
 	}
 
 }
